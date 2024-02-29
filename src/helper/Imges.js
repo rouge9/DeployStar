d546842b523d6ec92524f003696ca154fe878a6b
rouge9 <robelged1992@gmail.com>
2024-02-29 05:04:35 -0900

    init

---
+++ src/helper/Imges.js
@@ -0,0 +1,14 @@
+export const images = [
+  "/test.png",
+  "/test2.png",
+  "/test3.png",
+  "/test4.webp",
+  "/test5.png",
+  "/test6.webp",
+  "/test.png",
+  "/test2.png",
+  "/test3.png",
+  "/test4.webp",
+  "/test5.png",
+  "/test6.webp",
+];
